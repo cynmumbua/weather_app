f0bcabdd27be318f56c747a01e0a90c5d3be1d77
Cynthia Mumbua Muinde <cynmumbua@gmail.com>
2020-01-31 15:04:50 +0300

    Add Read me file Add select day Added fetch future data function "

--- main.js
+++ main.js
@@ -26,6 +26,7 @@ const iconValue = {
   PARTLY_CLOUDY_DAY: "partly-cloudy-day",
   PARTLY_CLOUDY_NIGHT: "partly-cloudy-night"
 };
+//fetch data from API current weather condition
 function fetchWeatherReport(apiKey, latitude, longitude) {
   let DsProxyLink = "https://cors-anywhere.herokuapp.com/";
   let dsApiLink = `${DsProxyLink}https://api.darksky.net/forecast/${apiKey}/${latitude},${longitude}?exclude=minutely,alerts,flags`;
@@ -35,21 +36,21 @@ function fetchWeatherReport(apiKey, latitude, longitude) {
       return response.json();
     })
     .then(data => {
-      var resultsHTML = "";
-      var tableHTML = "";
+      let resultsHTML = "";
+      let tableHTML = "";
       let summary = data.currently.summary;
-      let temperature = data.currently.temperature;
+      let temperature = ((data.currently.temperature - 32) * 5) / 9;
       let icon = data.currently.icon;
       let precipProbability = data.currently.precipProbability;
       let humidity = data.currently.humidity;
       let windSpeed = data.currently.windSpeed;
       let ts = new Date(data.currently.time * 1000);
+
       let forecastDate = `${wDay[ts.getDay()]} ${
         wMonth[ts.getMonth()]
       } ${ts.getDate()}`;
 
       //Set values for the current conditions
-      // document.getElementById("location").innerHTML = name;
       document.getElementById("dayTime").innerHTML = forecastDate;
       document.getElementById("summary").innerHTML = summary;
       document.getElementById("currentTemp").innerHTML = `${Math.round(
@@ -66,18 +67,18 @@ function fetchWeatherReport(apiKey, latitude, longitude) {
         windSpeed
       )} mph`;
 
-      document.getElementById("dailyForecast").innerHTML = getWeeklyForecast(
+      document.getElementById("weeklyForecast").innerHTML = getWeeklyForecast(
         data.daily
       );
-      document.getElementById("weeklyForecast").innerHTML = getDailyForecast(
-        data.hourly
+      document.getElementById("summaryDate").innerHTML = getSelectedDate(
+        data.daily
       );
     })
     .catch(err => {
       throw `error ${err} occured`;
     });
 }
-
+//displays the weeks weather condition
 function getWeeklyForecast(fData) {
   let resultsHTML =
     "<tr><th>Day</th><th>Conditions</th><th>Hi</th><th>Lo</th></tr>";
@@ -91,52 +92,48 @@ function getWeeklyForecast(fData) {
 
     let dayTime = wDay[ts.getDay()];
     let summary = fData.data[i].summary;
-    let tempHigh = `${Math.round(fData.data[i].temperatureHigh)}&deg`;
-    let tempLow = `${Math.round(fData.data[i].temperatureLow)}&deg`;
+    let tempHigh = `${Math.round(
+      ((fData.data[i].temperatureHigh - 32) * 5) / 9
+    )}&deg`;
+    let tempLow = `${Math.round(
+      ((fData.data[i].temperatureLow - 32) * 5) / 9
+    )}&deg`;
 
     resultsHTML += getRow(dayTime, summary, tempHigh, tempLow);
   }
 
   return resultsHTML;
 }
+
 function getRow(dayTime, summary, tempHigh, colVal4) {
   return `<tr><td>${dayTime}</td><td>${summary}</td><td>${tempHigh}</td><td>${colVal4}</td></tr>`;
 }
-function getDailyForecast(fData) {
-  let resultsHTML =
-    "<tr><th>Time</th><th>Conditions</th><th>Temp</th><th>Precip</th></tr>";
-  rowcount = fData.data.length;
-  if (rowcount > 8) {
-    rowcount = 8;
-  }
+//function fetch the data from API to get the future/past weather condition 
+function fetchFutureWeatherReport(apiKey, latitude, longitude) {
+  let strDate = document.getElementById("sDate").value;
+  let toTimestamp = Date.parse(strDate) / 1000;
+  console.log(toTimestamp);
+  let DsProxyLink = "https://cors-anywhere.herokuapp.com/";
+  let dsApiLink = `${DsProxyLink}https://api.darksky.net/forecast/9cf33b926d24e164a355f4df2d24ba9c/-1.1056331,37.0188397,${toTimestamp}?exclude=currently,flags`;
 
-  for (i = 0; i < rowcount; i++) {
-    let ts = new Date(fData.data[i].time * 1000);
-    let summary = "";
-    let tempHigh = 0;
-    let timeValue;
-
-    //unix time needs to be formatted for display
-    let hours = ts.getHours();
-    if (hours > 0 && hours <= 12) {
-      timeValue = "" + hours;
-    } else if (hours > 12) {
-      timeValue = "" + (hours - 12);
-    } else if (hours == 0) {
-      timeValue = "12";
-    }
-    timeValue += hours >= 12 ? " PM" : " AM"; // get AM/PM
-
-    summary = fData.data[i].summary;
-    tempHigh = `${Math.round(fData.data[i].temperature)}&deg`;
-    let precipProbability = `${Math.round(
-      fData.data[i].precipProbability * 100
-    )}%`;
-    resultsHTML += getRow(timeValue, summary, tempHigh, precipProbability);
-  }
-
-  return resultsHTML;
+  fetch(dsApiLink)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      let summary = data.daily.data[0].summary;
+      let temperature = ((data.daily.data[0].temperatureHigh - 32) * 5) / 9;
+      document.getElementById("summaryDate").innerHTML = summary;
+      document.getElementById("temperatureDate").innerHTML = `Temp: ${Math.round(
+        temperature
+      )}&deg`;
+    })
+    .catch(err => {
+      throw `error ${err} occured`;
+    });
 }
+
+//function to get name of the location
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(success, error);
@@ -144,15 +141,20 @@ function getLocation() {
     alert("geo location not supported");
   }
 }
-
+//function to get the lattitde and longitude of current location
 function success(position) {
   let lat = position.coords.latitude;
   let lng = position.coords.longitude;
   document.getElementById("location").innerHTML =
     "lat: " + lat + " lng: " + lng;
 
-  //   alert("lat: " + lat + " lng: " + lng);
   codeLatLng(lat, lng);
+  fetchFutureWeatherReport(
+    dsApiKey,
+    position.coords.latitude,
+    position.coords.longitude
+  );
+
   fetchWeatherReport(
     dsApiKey,
     position.coords.latitude,
@@ -184,30 +186,31 @@ function codeLatLng(lat, lng) {
     }
   });
 }
+//switch case function to get the desired icon
 function getICON(icon) {
   switch (icon) {
     case iconValue.CLEARDAY:
-      return "images/SunnyDay.png";
+      return "images/clear-day.svg";
 
     case iconValue.CLOUDY:
     case iconValue.PARTLY_CLOUDY_DAY:
-      return "images/MostlySunny.png";
+      return "images/partly-cloudy-day.svg";
 
     case iconValue.CLEARNIGHT:
-      return "images/ClearMoon.png";
+      return "images/clear-night.svg";
 
     case iconValue.PARTLY_CLOUDY_NIGHT:
-      return "images/CloudyMoon.png";
+      return "images/partly-cloudy-night.svg";
     case iconValue.RAIN:
-      return "images/Rain.png";
+      return "images/rain.svg";
 
     case iconValue.SNOW:
-      return "images/SNOW.png";
+      return "images/snow.svg";
 
     case iconValue.SLEET:
-      return "images/Sleet.png";
+      return "images/Sleet.svg";
 
     default:
-      return "images/SunnyDay.png";
+      return "images/clear-day.svg";
   }
 }
